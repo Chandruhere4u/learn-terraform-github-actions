397385755a03636fe91a3cf76e841d568179a2a3
Chandruhere4u <Chandruhere4u@gmail.com>
2022-09-13 12:48:39 -0400

    Changed the Terraform details

--- main.tf
+++ main.tf
@@ -12,16 +12,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ChandruTestOrg"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "TestTerraformGitActions1"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
